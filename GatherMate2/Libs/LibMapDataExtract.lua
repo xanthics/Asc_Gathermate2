45428f224b302324abe25b56615661b44f6ff110
xanthics <3137623+xanthics@users.noreply.github.com>
2025-10-17 20:18:23 -0700

    reworked logic for zone names

--- GatherMate2/Libs/LibMapDataExtract.lua
+++ GatherMate2/Libs/LibMapDataExtract.lua
@@ -10,16 +10,6 @@ local nametoid = {}
 local idtodxdy = {}
 local mapToLocal = {}
 
--- Build the localized name list.
-local continentList = {GetMapContinents()}
-for cID = 1, #continentList do
-	for zID, zname in ipairs({GetMapZones(cID)}) do
-		SetMapZoom(cID, zID)
-		local mapfile = GetMapInfo()
-		mapToLocal[mapfile] = zname
-	end
-end
-
 -- Don't know a better way
 -- Build list of areaIDs
 for i=0,9999 do
@@ -28,20 +18,27 @@ for i=0,9999 do
 		local x2, y2 = C_WorldMap.GetWorldPosition(i, 1, 1)
         local mapfile = C_WorldMap.GetMapFileByAreaID(i)
 		nametoid[mapfile] = i
-        -- if map isn't in overworld then save mapfile name.  Prevents issue with MapLocalize
-        if not mapToLocal[mapfile] then
-            mapToLocal[mapfile] = mapfile
-        end
-		idtodxdy[i] = { [1] = abs(x1-x2), [2] = abs(y1-y2) }
+        -- if map zoneid isn't valid then save mapfile name.  Prevents issue with MapLocalize
+		local zoneID = C_WorldMap.GetZoneIDByAreaID(i)
+		if zoneID > 0 then
+			mapToLocal[mapfile] = GetAreaName(zoneID)
+		else
+			mapToLocal[mapfile] = mapfile
+		end
+        idtodxdy[i] = { [1] = abs(x1-x2), [2] = abs(y1-y2) }
 	end
 end
 
 function GatherMate.mapData:MapLocalize(mapfile)
-	local t = mapfile
-	if type(mapfile) == "number" then
-		mapfile = C_WorldMap.GetMapFileByAreaID(mapfile)
-	end
 	if mapfile == WORLDMAP_COSMIC_ID then return WORLD_MAP end
+	if type(mapfile) == "number" then
+		local zoneID = C_WorldMap.GetZoneIDByAreaID(mapfile)
+		if zoneID > 0 and GetAreaName(zoneID)then
+			return GetAreaName(zoneID)
+		else
+			return C_WorldMap.GetMapFileByAreaID(mapfile)
+		end
+	end
     return mapToLocal[mapfile]
 end
 
