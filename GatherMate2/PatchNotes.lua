5635ebdce26e8a0308b9feaa1fd2830ede403463
xanthics <3137623+xanthics@users.noreply.github.com>
2025-11-11 12:18:21 -0800

    update patchnote

--- GatherMate2/PatchNotes.lua
+++ GatherMate2/PatchNotes.lua
@@ -4,6 +4,7 @@ local gatherNotes = {
         "Added patchnotes module from AtlasLoot",
         "Added Data Sharing Options",
         "Added Tracking for more HR nodes",
+        "Patched a bug with data importing so that you should now get all nodes",
 }
 
 function GatherMate:PatchNotes()
